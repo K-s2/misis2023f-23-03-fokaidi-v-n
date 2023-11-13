53f71446d78f1aeccb26ec6b0292df8dc7719017
K-s2 <81283622+K-s2@users.noreply.github.com>
2023-11-13 11:11:23 +0300

    Update and rename complex.cpp to complex.hpp

--- prj.lab/complex.hpp
+++ prj.lab/complex.hpp
@@ -174,42 +174,14 @@ std::istream& Complex::readFrom(std::istream& istrm) {
   return istrm;
 }
 
-// определяем поведение оператора << для Complex, мы вызываем функцию writeTo
+// определяем поведение перегруженного оператора << для Complex
 inline std::ostream& operator<<(std::ostream& ostrm, const Complex& rhs) {
   return rhs.writeTo(ostrm);
 }
 
-// определяем поведение оператора >> для Complex, мы вызываем функцию readFrom
+// определяем поведение пергруженного оператора >> для Complex
 inline std::istream& operator>>(std::istream& istrm, Complex& rhs) {
   return rhs.readFrom(istrm);
 }
 
-void operator_test_1() {
-  Complex x, y;
-  double d;
-  int e;
-  std::cout << "----------------------------" << std::endl;
 
-  std::cout << "\n \t operator + test \n" << std::endl;
-
-  x = Complex{ 2.5, 2.1 };
-  y = Complex{ 2.5, 3.5 };
-  std::cout << x << " + " << y << " = " << x + y << " Expected: {5, 5.6}\n";
-
-  x = Complex{ -2.5, 2.1 };
-  y = Complex{ 2.5, -3.5 };
-  std::cout << x << " x " << y << " = " << x + y << " Expected: {0, -1.4}\n";
-
-  d = 1.0;
-  x = Complex{ 2.0, -1.0 };
-  std::cout << x << " + " << d << " = " << x + d << " Expected: {3, -1}\n";
-
-  d = 1.6;
-  x = Complex{ 2.5, 1.6 };
-  std::cout << d << " + " << x << " = " << d + x << " Expected: {4.1, 1.6}\n";
-}
-int main()
-{
-  operator_test_1();
-  return 0;
-}
