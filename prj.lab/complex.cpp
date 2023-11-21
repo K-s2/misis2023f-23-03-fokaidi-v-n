b5d0b0ca51e705389ad153ae9b6cbda7de8d79ff
K-s2 <81283622+K-s2@users.noreply.github.com>
2023-11-21 22:35:48 +0300

    Update complex.cpp

--- prj.lab/complex.cpp
+++ prj.lab/complex.cpp
@@ -1,5 +1,4 @@
 #include "complex.hpp"
-//создаем блок перегруженных операторов 
 
 bool operator==(const Complex& lhs, const Complex& rhs) {
   return (lhs.re == rhs.re) && (lhs.im == rhs.im);
@@ -32,6 +31,7 @@ Complex operator+(const Complex& lhs, const Complex& rhs) {
 Complex operator-(const Complex& lhs, const Complex& rhs) {
   return Complex(lhs.re - rhs.re, lhs.im - rhs.im);
 }
+
 Complex& operator*=(Complex& lhs, const Complex& rhs) {
   double newRe = lhs.re * rhs.re - lhs.im * rhs.im;
   double newIm = lhs.re * rhs.im + lhs.im * rhs.re;
@@ -88,23 +88,22 @@ Complex pow(const Complex& lhs, const int rhs) {
   return result;
 }
 
-// определение функции writeTo
 std::ostream& Complex::writeTo(std::ostream& ostrm) const {
   ostrm << leftBrace << re << separator_comma << separator_space << im << rightBrace;
   return ostrm;
 }
 
-// определение функции readFrom 
 std::istream& Complex::readFrom(std::istream& istrm) {
   char leftBrace(0);
   double real(0.0);
   char comma(0);
+  char space(0);
   double imaginary(0.0);
   char rightBrace(0);
-  istrm >> leftBrace >> real >> comma >> imaginary >> rightBrace;
+  istrm >> leftBrace >> real >> comma >> space >> imaginary >> rightBrace;
   if (istrm.good())
   {
-    if ((Complex::leftBrace == leftBrace) && (Complex::separator_comma == comma) && (Complex::rightBrace == rightBrace))
+    if ((Complex::leftBrace == leftBrace) && (Complex::separator_comma == comma) && (Complex::separator_space == space) && (Complex::rightBrace == rightBrace))
     {
       re = real;
       im = imaginary;
@@ -117,15 +116,10 @@ std::istream& Complex::readFrom(std::istream& istrm) {
   return istrm;
 }
 
-// определяем поведение перегруженного оператора << для Complex
-inline std::ostream& operator<<(std::ostream& ostrm, const Complex& rhs) {
+std::ostream& operator<<(std::ostream& ostrm, const Complex& rhs) {
   return rhs.writeTo(ostrm);
 }
 
-// определяем поведение пергруженного оператора >> для Complex
-inline std::istream& operator>>(std::istream& istrm, Complex& rhs) {
+std::istream& operator>>(std::istream& istrm, Complex& rhs) {
   return rhs.readFrom(istrm);
 }
-
-
-
