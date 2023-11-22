77912262ecbcdea21e11a782b8d4656c6aff7815
K-s2 <81283622+K-s2@users.noreply.github.com>
2023-11-22 15:47:50 +0300

    Update rational.cpp

--- prj.lab/rational.cpp
+++ prj.lab/rational.cpp
@@ -1,1 +1,25 @@
 
+#include <sstream>
+
+class Rational {
+private:
+	int64_t num_ = 0;
+	int64_t den_ = 1;
+public:
+	//конструктор по умолчанию
+	Rational() : num_(0), den_(1) {}
+	//конструктор по умолчанию с одним аргументом
+	Rational(const int64_t numerator) : num_(numerator), den_(1) {}
+	//конструктор с двумя аргументами
+	Rational(const int64_t numerator, const int64_t denumerator) : num_(numerator), den_(denumerator) {}
+	
+	std::ostream& writeTo(std::ostream& ostrm) const;
+	std::istream& ReadFrom(std::istream& istrm);
+	int64_t
+};
+
+void test1() {
+	r1 = Rational(5 / 3);
+}
+
+
