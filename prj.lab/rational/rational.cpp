662d23855a7a367817c0e0ecd780fabafc7a0d0f
K-s2 <81283622+K-s2@users.noreply.github.com>
2023-12-10 22:27:42 +0300

    Update rational.cpp

--- prj.lab/rational/rational.cpp
+++ prj.lab/rational/rational.cpp
@@ -1,36 +1,94 @@
 #include "rational.hpp"
 
+Rational::Rational() : num_(0), den_(1) {}
+
+Rational::Rational(const int64_t numerator) : num_(numerator), den_(1) {}
+
+Rational::Rational(const int64_t numerator, const int64_t denumerator) : num_(numerator), den_(denumerator) {}
 
 std::ostream& Rational::WriteTo(std::ostream& ostrm) const {
   ostrm << num_ << separator_slash << den_;
   return ostrm;
 }
 
-inline std::ostream& operator<<(std::ostream& ostrm, const Rational& rhs) {
-  return rhs.WriteTo(ostrm);
+std::istream& Rational::ReadFrom(std::istream& istrm) {
+  char slash(0);
+  istrm >> num_ >> slash >> den_;
+  if (istrm && (separator_slash == slash)) {
+    gcd();
+  }
+  return istrm;
 }
 
-void test1() {
-  Rational r1;
-  r1 = Rational{ 5 , 2 };
-  std::cout << r1 << " ";
-  Rational r2;
-  r2 = Rational{ 1 , 2 };
-  r1 += r2;
-  std::cout << r1 << " ";
-  Rational r3{ 5, 2 };
-  r3 -= r2;
-  std::cout << r3;
-  Rational r4;
-  r4 = { 5, 2 };
-  r4 *= r2;
-  std::cout << " " << r4;
-  Rational r5;
-  r5 = { 5, 2 };
-  r5 /= r2;
-  std::cout << " " << r5;
+Rational& Rational::operator+=(const Rational& rhs) {
+  num_ = num_ * rhs.den_ + den_ * rhs.num_;
+  den_ = den_ * rhs.den_;
+  gcd();
+  return *this;
 }
 
-int main() {
-  test1();
+Rational& Rational::operator+=(const int64_t rhs) {
+  num_ = num_ + den_ * rhs;
+  gcd();
+  return *this;
+}
+
+Rational& Rational::operator-=(const Rational& rhs) {
+  num_ = num_ * rhs.den_ - den_ * rhs.num_;
+  den_ = den_ * rhs.den_;
+  gcd();
+  return *this;
+}
+
+Rational& Rational::operator-=(const int64_t rhs) {
+  num_ = num_ - rhs * den_;
+  gcd();
+  return *this;
+}
+
+Rational& Rational::operator*=(const Rational& rhs) {
+  num_ = num_ * rhs.num_;
+  den_ = den_ * rhs.den_;
+  gcd();
+  return *this;
+}
+
+Rational Rational::operator*=(const int64_t rhs) {
+  num_ = num_ * rhs;
+  gcd();
+  return *this;
+}
+
+Rational& Rational::operator/=(const Rational& rhs) {
+  num_ = num_ * rhs.den_;
+  den_ = den_ * rhs.num_;
+  gcd();
+  return *this;
+}
+
+Rational& Rational::operator/=(const int64_t rhs) {
+  den_ = den_* rhs;
+  gcd();
+  return *this;
+}
+
+int64_t Rational::num() const {
+  return num_;
+}
+
+int64_t Rational::den() const {
+  return den_;
+}
+
+int Rational::num_gcd(int64_t a, int64_t b) {
+  if (b == 0) {
+    return a;
+  }
+  return num_gcd(b, a % b);
+}
+
+void Rational::gcd() {
+  int64_t gcd_ = num_gcd(num_, den_);
+  num_ = num_ / gcd_;
+  den_ = den_ / gcd_;
 }
